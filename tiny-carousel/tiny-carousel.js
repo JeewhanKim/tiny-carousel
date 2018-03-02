b15ecc43ec24718571c01df154c9f7a5dc771baf
Jeewhan Kim <awryong@gmail.com>
2018-03-02 13:05:25 -0500

    initial setup

---
+++ tiny-carousel/tiny-carousel.js
@@ -0,0 +1,10 @@
+/*
+  🎠 Tiny Carousel 🎠
+  Light-weight, simple, and easy-to-use Javascript plugin for slider functionality ⌛️
+  Version:  0.5.0
+  Author:   Jeewhan Kim
+  Home:     https://github.com/JeewhanKim
+*/
+(function(window, document){
+  console.log('tiny-carousel.js has been imported')
+})(window, document)
